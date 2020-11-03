c9e0423e909c86abfe0cb1655e03ccf4627d6074
g3sporter <52547590+g3sporter@users.noreply.github.com>
2020-11-03 13:57:22 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+provider "google" {
+ #credentials = "${file("/Users/bryan/hashicorp/bryanTest-d87f1249d43c.json")}"
+ #project     = "bryanTest"
+  region      = "us-west1"
+}
+
 resource "google_compute_instance" "default" {
 
   name         = "test"
